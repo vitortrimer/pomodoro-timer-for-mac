6b5b25f431e05689a2f8a5ae368be088eb75a108
Vitor Trimer <vitortrimer@gmail.com>
2023-01-27 20:01:29 -0300

    Initial Commit

---
+++ Pomodoro/PomodoroApp.swift
@@ -0,0 +1,17 @@
+//
+//  PomodoroApp.swift
+//  Pomodoro
+//
+//  Created by Vitor Trimer on 27/01/23.
+//
+
+import SwiftUI
+
+@main
+struct PomodoroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
