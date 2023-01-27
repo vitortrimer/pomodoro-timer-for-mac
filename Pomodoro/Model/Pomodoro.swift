9921899f4002559aa2012ba48f02e394703d36a3
Vitor Trimer <vitortrimer@gmail.com>
2023-01-27 20:17:23 -0300

    add structure

---
+++ Pomodoro/Model/Pomodoro.swift
@@ -0,0 +1,15 @@
+//
+//  Pomodoro.swift
+//  Pomodoro
+//
+//  Created by Vitor Trimer on 27/01/23.
+//
+
+import Foundation
+
+struct Pomodoro: Identifiable, Hashable, Equatable {
+    var id: UUID = .init()
+    var name: String
+    var focusTime: String
+    var restTime: String
+}
---
+++ Pomodoro/View/Home.swift
@@ -0,0 +1,20 @@
+//
+//  Home.swift
+//  Pomodoro
+//
+//  Created by Vitor Trimer on 27/01/23.
+//
+
+import SwiftUI
+
+struct Home: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Home_Previews: PreviewProvider {
+    static var previews: some View {
+        Home()
+    }
+}
