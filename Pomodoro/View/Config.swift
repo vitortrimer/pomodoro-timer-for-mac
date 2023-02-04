50b35c5ffad182d8a4d5b90d3bbc747a79bff8fa
Vitor Trimer <vitortrimer@gmail.com>
2023-02-04 20:47:52 -0300

    Add slider

--- Pomodoro/View/Config.swift
+++ Pomodoro/View/Config.swift
@@ -8,11 +8,41 @@
 import SwiftUI
 
 struct Config: View {
+    
+    @State private var speed = 0.0
+    
     var body: some View {
-        VStack {
-            
-            Text("Hello, World!")
+        VStack(alignment: .leading, spacing: 16) {
+            VStack {
+                TimePickerConfig(pickerType: .focus)
+            }
+            Text("Time - \(speed)")
         }
+    }
+    
+    
+    @ViewBuilder
+    private func TimePickerConfig(pickerType type: PickerType) -> some View {
+        VStack(alignment: .leading, spacing: 8) {
+            Text("\(type.rawValue):")
+            Slider(
+                value: $speed,
+                in: 0...60,
+                step: 1
+            ) {
+            }
+            minimumValueLabel: {
+                Text("0")
+            } maximumValueLabel: {
+                Text("60 minutes")
+            }
+            
+        }
+    }
+    
+    private enum PickerType: String {
+        case rest = "Rest time"
+        case focus = "Focus time"
     }
 }
 
